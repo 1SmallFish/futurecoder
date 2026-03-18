6bb8bc60192c8bdd9df7a09cc0140a4992d991fb
lxh <scrcpy@foxmail.com>
2026-03-18 10:58:59 +0800

    ./scripts/build.sh: line 17: FUTURECODER_LANGUAGE: unbound variable 这个变量在。env已经定义

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,7 +14,8 @@ poetry run python -m scripts.generate_static_files
 
 # Build the react app in the frontend folder, 'compiling' JS and CSS, and copy the result into the dist folder
 cd frontend
-REACT_APP_PRECACHE=1 REACT_APP_LANGUAGE=$FUTURECODER_LANGUAGE CI=false npm run build
+# REACT_APP_LANGUAGE=$FUTURECODER_LANGUAGE
+REACT_APP_PRECACHE=1  CI=false npm run build
 cd ..
 cp -r frontend/course/* dist/course/
 
